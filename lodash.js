ce477a370472999ba4b54e7c7054bd0b4200fecd
Jarno Rantanen <jarno@jrw.fi>
2016-01-06 00:22:03 +0200

    Update _.slice() docs again
    Update note to point to the standard Array.slice().

--- lodash.js
+++ lodash.js
@@ -6679,15 +6679,16 @@
     /**
      * Creates a slice of `array` from `start` up to, but not including, `end`.
      *
-     * **Note:** This method is used instead of [`Array#slice`](https://mdn.io/Array/slice)
-     * to ensure dense arrays are returned.
+     * **Note:** This method is based on [`Array#slice`](https://mdn.io/Array/slice),
+     * but uses our own implementation to ensure dense arrays are returned.
+     * This means e.g. negative `start` positions work as expected.
      *
      * @static
      * @memberOf _
      * @since 3.0.0
      * @category Array
      * @param {Array} array The array to slice.
-     * @param {number} [start=0] The start position. Negative values allow starting relative to the end of the array.
+     * @param {number} [start=0] The start position.
      * @param {number} [end=array.length] The end position.
      * @returns {Array} Returns the slice of `array`.
      */
